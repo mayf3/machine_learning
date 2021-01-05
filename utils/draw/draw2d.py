c2bd9e9c1b3b5753a5d86d4a5a296141c269eec0
yanfenma <daretobeyourself.m.y.f@gmail.com>
2021-01-05 09:44:00 +0800

    Add draw perceptron

--- utils/draw/draw2d.py
+++ utils/draw/draw2d.py
@@ -4,10 +4,10 @@ import matplotlib.pyplot as plt
 
 
 # Prepare data
-x_of_first_type = [1, 2, 4, 5, 8]
-y_of_first_type = [1, 2, 4, 5, 8]
-x_of_second_type = [3, 6, 7, 9, 10]
-y_of_second_type = [3, 6, 7, 9, 10]
+x_of_first_type = [1, 2, 1, 2, 3]
+y_of_first_type = [1, 2, 3, 1, 1]
+x_of_second_type = [4, 5, 3, 4, 5]
+y_of_second_type = [1, 2, 2, 2, 3]
 #plt.plot(x_of_first_type, y_of_first_type, marker="-", color='b', label='first type')
 #plt.plot(x_of_second_type, y_of_second_type, marker="--", color='g', label='second type')
 
@@ -16,6 +16,7 @@ for i in range(len(x_of_first_type)):
 for i in range(len(x_of_second_type)):
     plt.plot([x_of_second_type[i]], [y_of_second_type[i]], marker='o', color='g')
 
+plt.plot([1, 4], [4, 0], color='r')
 # test_x = 3.2
 # test_y = 3.2
 # plt.plot([test_x], [test_y], marker='.', color='r')
@@ -29,8 +30,8 @@ plt.xlabel("X")
 plt.ylabel("Y")
 
 # Set the range of coordinate
-plt.xlim(0, 11)
-plt.ylim(0, 11)
+plt.xlim(0, 6)
+plt.ylim(0, 4)
 
 # Show image
 plt.legend()
